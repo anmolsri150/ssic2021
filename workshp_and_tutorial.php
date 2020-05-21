714e12bf43a25fe4b5c026acaee8321951f43c1e
yoloabhi <abhirup_cool2@yahoo.com>
2020-05-22 00:56:24 +0530

    Update workshp_and_tutorial.php

--- workshp_and_tutorial.php
+++ workshp_and_tutorial.php
@@ -37,6 +37,15 @@
     <!-- Main Header-->
     <?php include 'header.php';?>
     <!--End Main Header -->
+    <style>
+        .icon-dots {
+            visibility: hidden!important;
+        }
+        p{
+            color:rgba(0,0,0,6);
+        }
+       
+    </style>
 
     <!--Page Title-->
     <section class="page-title" style="background-image:url(https://thessic.com/images/background/5.jpg);">
@@ -59,10 +68,148 @@
             <span class="icon icon-circle-1 wow zoomIn"></span>
         </div>
 
-        <div class="auto-container">
-            <div class="error-title">404</div>
-            <h4>Page not Found</h4>
-            <div class="text">Sorry, we couldn't find the page you're looking for</div>
+        <div class="auto-container"style="text-align: left!important;">
+<!--            <div class="error-title">404</div>-->
+<!--            <h4>Page not Found</h4>-->
+<!--            <div class="text">Sorry, we couldn't find the page you're looking for</div>-->
+    
+            <div class="error-title" style="font-size: 45px; margin-bottom: 30px;">Call for WORKSHOP or Tutorial</div>
+    
+            <div class="error-title" style="font-size: 40px;">Proposal Submissions Guidelines</div>
+    
+            <hr /><p>SSIC 2021 will be hosting a series of workshops and tutorials. Workshops and tutorials feature topics relevant to the Conference theme on the latest research, engineering, standards and business issues. </p>
+    
+            <p>The objective of a Scientific Workshop at SSIC 2021 is to give researchers the opportunity to present and receive feedback on early research results. The objective of an Industrial Workshop is to create an environment for smart system pioneers and practitioners in industry to share hands - on experience with computing systems. </p>
+    
+            <p>Tutorials are supposed to teach participants hands - on practical knowledge about emerging innovative computational techniques and enable them to use the learned technology for their own projects.</p>
+    
+            <div class="error-title" style="font-size: 35px;margin-bottom: 30px; margin-top: 20px;">How to Apply</div>
+    
+            <p><strong>Each proposal must include the following applications and should be maximum five pages.</strong></p>
+    
+            <p><strong>Theme for Workshop or Tutorial (not limited to)</strong></p>
+    
+            <ol >
+                <li><p>1). &nbsp Software Engineering for Smart Systems</p>
+    
+                </li>
+                <li><p>2). &nbsp Affective Computing</p>
+        
+                </li>
+            
+    
+            <li><p>3). &nbsp Agriculture Automation</p>
+        
+                </li>
+            
+    
+            <li><p>4). &nbsp Applied Electromagnetic in Smart Systems</p>
+        
+                </li>
+            
+    
+            <li><p>5). &nbsp Biometrics and Smart Systems</p>
+        
+                </li>
+            
+            <li><p>6). &nbsp Cloud Computing</p>
+        
+                </li>
+            <li><p>7). &nbsp Communication Systems</p>
+        
+                </li>
+            <li><p>8). &nbsp Computational Intelligence</p>
+        
+                </li>
+           <li><p>9). &nbsp Control Systems and Robotics</p>
+        
+                </li>
+            <li><p>10). &nbsp Cyber-Physical Systems</p>
+        
+                </li>
+            <li><p>11). &nbsp Dependable Computer Systems</p>
+        
+                </li>
+            <li><p>12). &nbsp Embedded Computer Systems</p>
+        
+                </li>
+            <li><p>13). &nbsp Green Communication</p>
+        
+                </li>
+            <li><p>14). &nbsp Green Computing</p>
+        
+                </li>
+            <li><p>15). &nbsp High-Performance Computing</p>
+        
+                </li>
+            <li><p>16). &nbsp Image and Video Processing</p>
+        
+                </li>
+            <li><p>17). &nbsp Intelligent Power Systems and Environments</p>
+        
+                </li>
+            <li><p>18). &nbsp Intelligent Transportation Systems</p>
+        
+                </li>
+            <li><p>19). &nbsp Internet of Things</p>
+        
+                </li>
+            <li><p>20). &nbsp Mobile and Wireless Networks</p>
+        
+                </li>
+           <li><p>21). &nbsp Security and Privacy</p>
+        
+                </li>
+            <li><p>22). &nbsp Smart Antennas</p>
+        
+                </li>
+           <li><p>23). &nbsp Smart Health</p>
+        
+                </li>
+            <li><p>24). &nbsp Smart Houses and Cities</p>
+        
+                </li>
+            <li><p>25). &nbsp Smart Systems and Technologies in Education</p>
+        
+                </li>
+            <li><p>26). &nbsp Smart Systems for Green Energy</p>
+        
+                </li>
+            <li><p>27). &nbsp Smart Systems in Business Information Systems</p>
+        
+                </li>
+           
+            </ol>
+    
+            <h4 style="font-size: 20px">Workshop and tutorial Application: <a href="https://thessic.com/resources/workshop_proposal_form_ssic_2021.pdf" ><span class="btn-title">Click Here    </span></a>(href: attached pdf file)</h4>
+    
+            <p><strong>Workshop or Tutorial proposals must be submitted via email to the SSIC:</strong><a href="about:blank" > ssic.muj@gmail.com</a> and <a href="mailto:ankit.mundra@jaipur.manipal.edu" >ankit.mundra@jaipur.manipal.edu</a> with “Application for Workshop/Tutorial the SSIC-2021 Conference” written in the subject line of your message.</p>
+    
+            <p><strong><br>
+                </strong></p>
+    
+            <div class="error-title" style="font-size: 27px;margin-bottom: 30px;">Important Dates for Proposal Submissions: </div>
+    
+            <p><strong> DEADLINE::</strong> 30th July, 2020</p>
+    
+            <p>For more details please visit the conference website (<a href="http://www.thessic.com" >www.thessic.com</a> ) </p>
+    
+            <p><strong><br>
+                </strong></p>
+    
+            <div class="error-title" style="font-size: 27px;margin-bottom: 30px;">Contact Person for Special Session:</div>
+            
+    
+            <ol ><li><p>Ankit Mundra (<a href="mailto:ankit.mundra@jaipur.manipal.edu" >ankit.mundra@jaipur.manipal.edu</a>)</p>
+        
+                </li>
+                <li><p>Rohit Kumar Gupta (<a href="mailto:rohitkumar.gupta@jaipur.manipal.edu" >rohitkumar.gupta@jaipur.manipal.edu</a>)</p>
+        
+                </li>
+                <li><p>Vivek Kumar Verma (<a href="mailto:Vivekkumar.verma@jaipur.manipal.edu" >Vivekkumar.verma@jaipur.manipal.edu</a></p>
+        
+                </li>
+            </ol>
             <a href="https://thessic.com" class="theme-btn btn-style-three"><span class="btn-title">Home Page</span></a>
         </div>
     </section>
