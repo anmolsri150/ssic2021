e6f1c85a1bd8327f7ec5cac3c6be9cf12b0e433b
Anmol Srivastava <anmol150srivastava@gmail.com>
2020-05-21 23:04:51 +0530

    added new pages

---
+++ special_session.php
@@ -0,0 +1,89 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <title>SSIC 2021 | Manipal University Jaipur</title>
+<!-- Stylesheets -->
+<link href="https://thessic.com/css/bootstrap.css" rel="stylesheet">
+<link href="https://thessic.com/css/style.css" rel="stylesheet">
+<link href="https://thessic.com/css/responsive.css" rel="stylesheet">
+<!--Color Switcher Mockup-->
+<link href="https://thessic.com/css/color-switcher-design.css" rel="stylesheet">
+
+
+<link rel="shortcut icon" href="https://thessic.com/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="https://thessic.com/images/favicon.png" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="https://thessic.com/js/respond.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+<div class="page-wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader"></div>
+ 	<!-- Header span -->
+
+    <!-- Header Span -->
+    <span class="header-span"></span>
+
+    <!-- Main Header-->
+    <?php include 'header.php';?>
+    <!--End Main Header -->
+
+    <!--Page Title-->
+    <section class="page-title" style="background-image:url(https://thessic.com/images/background/5.jpg);">
+        <div class="auto-container">
+            <h1>Error Page</h1>
+            <ul class="bread-crumb clearfix">
+                <li><a href="index.html">Home</a></li>
+                <li>Error Page</li>
+            </ul>
+        </div>
+    </section>
+    <!--End Page Title-->
+
+    <!--Error Section-->
+    <section class="error-section">
+        <div class="anim-icons full-width">
+            <span class="icon icon-circle-blue wow fadeIn"></span>
+            <span class="icon icon-dots wow fadeInleft"></span>
+            <span class="icon icon-line-1 wow zoomIn"></span>
+            <span class="icon icon-circle-1 wow zoomIn"></span>
+        </div>
+
+        <div class="auto-container">
+            <div class="error-title">404</div>
+            <h4>Page not Found</h4>
+            <div class="text">Sorry, we couldn't find the page you're looking for</div>
+            <a href="https://thessic.com" class="theme-btn btn-style-three"><span class="btn-title">Home Page</span></a>
+        </div>
+    </section>
+    <!--Error Section-->
+
+    <!-- Main Footer -->
+    
+    <?php include 'footer.php';?>
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-double-up"></span></div>
+<script src="https://thessic.com/js/jquery.js"></script>
+<script src="https://thessic.com/js/popper.min.js"></script>
+<script src="https://thessic.com/js/bootstrap.min.js"></script>
+<script src="https://thessic.com/js/jquery-ui.js"></script>
+<script src="https://thessic.com/js/jquery.fancybox.js"></script>
+<script src="https://thessic.com/js/appear.js"></script>
+<script src="https://thessic.com/js/owl.js"></script>
+<script src="https://thessic.com/js/wow.js"></script>
+<script src="https://thessic.com/js/script.js"></script>
+<!-- Color Setting -->
+<script src="https://thessic.com/js/color-settings.js"></script>
+</body>
+</html>
