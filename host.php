889b276bc7bead8cd572077645f66cfce5af1d3b
yoloabhi <abhirup_cool2@yahoo.com>
2020-05-21 23:56:53 +0530

    Update host.php

--- host.php
+++ host.php
@@ -41,14 +41,19 @@
     <!--Page Title-->
     <section class="page-title" style="background-image:url(https://thessic.com/images/background/5.jpg);">
         <div class="auto-container">
-            <h1>Error Page</h1>
+            <h1>Call for Hosts SSIC 2022</h1>
             <ul class="bread-crumb clearfix">
-                <li><a href="index.html">Home</a></li>
-                <li>Error Page</li>
+                <li><a href="https://thessic.com/">Home</a></li>
+                
             </ul>
         </div>
     </section>
     <!--End Page Title-->
+    <style>
+        .icon-dots {
+            visibility: hidden!important;
+        }
+    </style>
 
     <!--Error Section-->
     <section class="error-section">
@@ -59,10 +64,116 @@
             <span class="icon icon-circle-1 wow zoomIn"></span>
         </div>
 
-        <div class="auto-container">
-            <div class="error-title">404</div>
-            <h4>Page not Found</h4>
-            <div class="text">Sorry, we couldn't find the page you're looking for</div>
+        <div class="auto-container" >
+            <div class="error-title" style="font-size: 45px; margin-bottom: 30px;">CALL FOR HOSTING SSIC-2022</div>
+
+    
+            <div class="error-title" style="font-size: 40px;">4th International Conference SSIC-2022</div>
+    
+            <hr /><p>The International Conference SSIC brings together researchers and specialists in the area of smart systems and innovation in computing since 2017. SSIC highlights the various aspects of computer science with special focus on the high-performance computing, IOT, smart systems and security etc. <br>Information about previous editions of the conference can be found on the web pages for SSIC-2017 (<a href="https://www.thessic.com/ssic.html" >https://www.thessic.com/ssic.html</a>), SSIC-2019 (<a href="https://www.thessic.com/ssic2019.html" >https://www.thessic.com/ssic2019.html</a>) and next version SSIC-2021 (<a href="https://www.thessic.com/about-us.html" >https://www.thessic.com/about-us.html</a> ) .
+    
+                The SSIC core committee invites proposals from institutes for hosting the 4th International conference in 2022. The preferred conference date is during 1st or 2nd week of January 2022. Other nearby dates can be discussed. The committee expects the co-organizing institution to bear some responsibilities of managing the conference, and hence a group of active organizers is most welcome.</p>
+    
+                <br><br><h4 style="font-size: 16px;">THE SCHEDULE FOR THE SELECTION OF THE HOSTING OF THE 4th SSIC 2022 IS As FOLLOWS: </h4>
+    
+            <table ><colgroup><col ><col ></colgroup>
+                <tbody>
+                    <tr>
+                        <td><p>Announcement of Call for Proposals</p></td>
+                        <td><p>01-May-2020</p></td>
+                    </tr>
+            
+                    <tr>
+                        <td><p>Submission of Proposals</p></td>
+                        <td><p>30-Aug-2020</p></td>
+                    </tr>
+            
+                    <tr>
+                        <td><p>Discussion with applicants of feasible proposals</p></td>
+                        <td><p>1-Sep-2020 to 20-Sep-2020</p></td>
+                    </tr>
+            
+                    <tr>
+                        <td><p>Final decision</p></td>
+                        <td><p>01-Oct-2020</p></td>
+                    </tr>
+            
+                    <tr>
+                        <td><p>Site Inspections</p></td>
+                        <td><p>Oct – Nov ,2020</p></td>
+                    </tr>
+            
+                    <tr>
+                        <td><p>Finalist presentations: during the SSIC-2021</p></td>
+                        <td><p>22-23 January,2021</p></td>
+                    </tr>
+            
+                    <tr>
+                        <td><p>Selection of the Host by the SSIC Core committee</p></td>
+                        <td><p>23-January, 2021</p></td>
+                    </tr>
+        
+                </tbody>
+            </table>
+    
+            <div class="error-title" style="font-size: 35px;margin-bottom: 30px; margin-top: 20px;">How to Apply</div>
+    
+            <p>Send your application via email to the SSIC:<a href="about:blank" > ssic.muj@gmail.com</a> and anki.mundra@jaipur.manipal.edu with "Application for hosting the SSIC-2022 Conference" written in the subject line of your message.</p>
+    
+            <p>For more details please visit the conference website (<a href="http://www.thessic.com" >www.thessic.com</a> ) </p>
+    
+            <br>
+            <div class="error-title" style="font-size: 35px;margin-bottom: 30px;">PROPOSAL REQUIREMENTS</div>
+    
+            <h4 style="font-size: 18px;">Proposals for hosting the SSIC 2022 Conference should consist of a 3-5 page document that includes the following information: </h4>
+    
+            <ol style="text-align: left; margin-left: 90px;"<li><p>1.) The Professional Curriculum Vitae of the applicant (who will serve as one of the Programme Co-Chairs if proposal is accepted). </p>
+        
+                </li>
+            
+    
+            <li><p>2.) Description of transportation options (nearest airports, transportation from airport to campus, transportation from campus to housing/hotels)</p>
+        
+                </li>
+    
+            <li><p>3.) Description of lodging options (local hotels/motels, on-campus housing,)</p>
+        
+                </li>
+           
+    
+            <li><p>4.) Description of available conference facilities including more detailed information. </p>
+        
+                </li>
+            
+    
+            <li><p>5.) A brief explanation on why the campus wants to host the SSIC Conference </p>
+        
+                </li>
+    
+            <li><p>6.) Listing of potential service-related partners on campus and in the community (as an attachment) </p>
+        
+                </li>
+           
+            <li><p>7.) (optional) A scanned copy of the letter / email from the head of the department/centre/ laboratory proposing to host the conference, which should be forwarded and approved by head of the institute (Director/Dean/Registrar). </p>
+        
+                </li>
+            </ol><br>
+            <<div class="error-title" style="font-size: 27px;margin-bottom: 30px;">Contact Person for Special Session:</div>
+    
+            <br>
+            <ol ><li><p>Ankit Mundra (<a href="mailto:ankit.mundra@jaipur.manipal.edu" >ankit.mundra@jaipur.manipal.edu</a>)</p>
+        
+                </li>
+                <li><p>Rohit Kumar Gupta (<a href="mailto:rohitkumar.gupta@jaipur.manipal.edu" >rohitkumar.gupta@jaipur.manipal.edu</a>)</p>
+        
+                </li>
+                <li><p>Vivek Kumar Verma (<a href="mailto:Vivekkumar.verma@jaipur.manipal.edu" >Vivekkumar.verma@jaipur.manipal.edu</a> )</p>
+        
+                </li>
+            </ol><br>
+<!--            <div class="error-title">404</div>-->
+<!--            <h4>Page not Found</h4>-->
+<!--            <div class="text">Sorry, we couldn't find the page you're looking for</div>-->
             <a href="https://thessic.com" class="theme-btn btn-style-three"><span class="btn-title">Home Page</span></a>
         </div>
     </section>
