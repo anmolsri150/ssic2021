0389f5950db33829a76b5c6baaa472803780c6c5
yoloabhi <abhirup_cool2@yahoo.com>
2020-06-15 02:02:01 +0530

    Mailer added

--- home.php
+++ home.php
@@ -1143,13 +1143,15 @@
                     <div class="form-column col-lg-6 col-md-12">
                         <!--Newsletter Form-->
                         <div class="newsletter-form">
-                            <form method="post" action="blog.html">
+                            <form action="https://gmail.us10.list-manage.com/subscribe/post?u=6f3230c2fe293db4849ffbbb0&amp;id=c7273518f3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                                 <div class="form-group">
-                                    <input type="email" name="field-name" value="" placeholder="Enter Your Email" required="">
-                                    <button type="submit" class="theme-btn btn-style-three"><span class="btn-title">Subscribe</span></button>
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    
+                                    
+                                    <button type="submit" class="theme-btn btn-style-three" name="subscribe" id="mc-embedded-subscribe" value="Subscribe"><span class="btn-title">Subscribe</span></button>
                                 </div>
                             </form>
-                        </div>
+                          
                     </div>
 
                 </div>
@@ -1164,6 +1166,7 @@
     <!--End pagewrapper-->
 <!--Scroll to top-->
 <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-double-up"></span></div>
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 <script src="https://thessic.com/js/jquery.js"></script>
 <script src="https://thessic.com/js/popper.min.js"></script>
 <script src="https://thessic.com/js/bootstrap.min.js"></script>
