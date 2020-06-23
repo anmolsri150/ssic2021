7a2c824f5b5f004aa141c2a2b44ef1cd268fc6ba
akshaj1999 <akshajjuneja9@gmail.com>
2020-06-23 22:24:40 +0530

    covid page

---
+++ covid19.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <title>SSIC 2021 | Manipal University Jaipur</title>
+<!-- Stylesheets -->
+<link href="https://thessic.com/css/bootstrap.css" rel="stylesheet">
+<link href="https://thessic.com/css/style.css" rel="stylesheet">
+<link href="https://thessic.com/css/responsive.css" rel="stylesheet">
+<!--Color Switcher Mockup-->
+<link href="https://thessic.com/css/color-switcher-design.css" rel="stylesheet">
+
+
+<link rel="shortcut icon" href="https://thessic.com/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="https://thessic.com/images/favicon.png" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="https://thessic.com/js/respond.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+<div class="page-wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader"></div>
+ 	<!-- Header span -->
+
+    <!-- Header Span -->
+    <span class="header-span"></span>
+
+    <!-- Main Header-->
+    <?php include 'header.php';?>
+    <!--End Main Header -->
+    <style>
+        .icon-dots {
+            visibility: hidden!important;
+        }
+        p{
+            color:rgba(0,0,0,6);
+        }
+        .error-title {
+            text-align: center;
+        }
+       
+    </style>
+
+    <!--Page Title-->
+    <section class="page-title" style="background-image:url(https://thessic.com/images/background/5.jpg);">
+        <div class="auto-container">
+            <h1>Call for Workshops and Tutorials</h1>
+            <ul class="bread-crumb clearfix">
+                <li><a href="https://thessic.com/">Home</a></li>
+                <li>COVID-19</li>
+            </ul>
+        </div>
+    </section>
+    <!--End Page Title-->
+
+    <!--Error Section-->
+    <section class="error-section">
+        <div class="anim-icons full-width">
+            <span class="icon icon-circle-blue wow fadeIn"></span>
+            <span class="icon icon-dots wow fadeInleft"></span>
+            <span class="icon icon-line-1 wow zoomIn"></span>
+            <span class="icon icon-circle-1 wow zoomIn"></span>
+        </div>
+
+        <div class="auto-container"style="text-align: justify;">
+<!--            <div class="error-title">404</div>-->
+<!--            <h4>Page not Found</h4>-->
+<!--            <div class="text">Sorry, we couldn't find the page you're looking for</div>-->
+    
+            <div class="error-title" style="font-size: 40px;">Smart Striving Solutions to Combat with COVID-19</div>
+    
+            <hr /><p>The Researchers all around the world are currently addressing challenges in the face of the COVID-19 pandemic that require smart solutions to address the current challenges in society.</p>
+    
+            <p>Researchers in medicine, public health, computing, informatics, data science, and many other disciplines are conscientiously using their knowledge to work together to combat with this pandemic.</p> 
+            
+            <p> In the middle of this crisis, our aims to encourage researchers from industry as well as academia to discover the smart solutions and combat with COVID-19 pandemic through incorporating IoT-based solutions. </p>
+    
+            <div class="error-title" style="font-size: 35px;margin-bottom: 30px; margin-top: 20px;">How to Apply</div>
+        
+            <p><strong>We invite authors to submit their latest work and share their understanding with in this challenging period.</strong></p>
+    
+            <p><strong>The topics in this special session include the following:</strong></p>
+    
+            <ol >
+                <li><p>1). &nbsp Smart  Healthcare</p>
+    
+                </li>
+                <li><p>2). &nbsp Smart Risk Forecasting </p>
+        
+                </li>
+            
+    
+            <li><p>3). &nbsp Smart Emergency Management </p>
+        
+                </li>
+            
+    
+            <li><p>4). &nbsp Smart Resource Allocation</p>
+        
+                </li>
+            
+    
+            <li><p>5). Smart Logistics and Industry</p>
+        
+                </li>
+            
+            <li><p>6). &nbsp Internet of Medical Things (IoMT)</p>
+        
+                </li>
+            <li><p>7). &nbsp Smart Broadcast Management for Pandemic Monitoring</p>
+        
+                </li>
+            <li><p>8). &nbsp Community Impact</p>
+        
+                </li>
+           <li><p>9). &nbsp Smart Network collaboration </p>
+        
+                </li>
+            
+            </ol>
+        </div>
+    </section>
+    <!--Error Section-->
+
+    <!-- Main Footer -->
+    
+    <?php include 'footer.php';?>
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-double-up"></span></div>
+<script src="https://thessic.com/js/jquery.js"></script>
+<script src="https://thessic.com/js/popper.min.js"></script>
+<script src="https://thessic.com/js/bootstrap.min.js"></script>
+<script src="https://thessic.com/js/jquery-ui.js"></script>
+<script src="https://thessic.com/js/jquery.fancybox.js"></script>
+<script src="https://thessic.com/js/appear.js"></script>
+<script src="https://thessic.com/js/owl.js"></script>
+<script src="https://thessic.com/js/wow.js"></script>
+<script src="https://thessic.com/js/script.js"></script>
+<!-- Color Setting -->
+<script src="https://thessic.com/js/color-settings.js"></script>
+</body>
+</html>
